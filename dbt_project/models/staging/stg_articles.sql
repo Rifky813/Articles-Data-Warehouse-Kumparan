59a1de4fad9467b7be2ce63ec23c36058fd27228
Rifky <rifkyyudistiansyah@gmail.com>
2026-06-21 07:30:05 +0700

    Add data quality check with dbt test

--- dbt_project/models/staging/stg_articles.sql
+++ dbt_project/models/staging/stg_articles.sql
@@ -1,11 +1,12 @@
 {{ config(materialized='view') }}
 
 SELECT
-    id AS article_id,
-    title,
-    content,
-    published_at,
-    author_id,
-    created_at,
-    updated_at
+CAST(id AS UInt64) AS article_id,
+    CAST(author_id AS UInt64) AS author_id,
+    TRIM(CAST(title AS String)) AS title,
+    CAST(content AS String) AS content,
+    CAST(published_at AS DateTime) AS published_at,
+    CAST(created_at AS DateTime) AS created_at,
+    CAST(updated_at AS DateTime) AS updated_at
 FROM {{ source('default', 'raw_articles') }}
+WHERE id IS NOT NULL
--- dbt_project/models/staging/stg_deleted_articles.sql
+++ dbt_project/models/staging/stg_deleted_articles.sql
@@ -1,6 +1,7 @@
 {{ config(materialized='view') }}
 
 SELECT
-    article_id,
-    deleted_at
+    CAST(article_id AS UInt64) AS article_id,
+    CAST(deleted_at AS DateTime) AS deleted_at
 FROM {{ source('default', 'raw_deleted_log') }}
+WHERE article_id IS NOT NULL
